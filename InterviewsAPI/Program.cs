7dbdfc8a5bbba397d171bb8dfebbb2c28482ecb7
Alexey Taradoyna <AltarNatsev@yandex.ru>
2024-03-17 16:28:29 +0700

    init: Migration Db was performed using EF

--- InterviewsAPI/Program.cs
+++ InterviewsAPI/Program.cs
@@ -1,3 +1,6 @@
+using InterviewsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<InterviewDbContect>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
