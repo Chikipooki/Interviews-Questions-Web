978fc203915eee4e1cad4c50e5bb863c9c7a3e76
Alexey Taradoyna <AltarNatsev@yandex.ru>
2024-03-17 16:27:00 +0700

    fix: Fix typos in data types

--- InterviewsAPI/Models/Participant.cs
+++ InterviewsAPI/Models/Participant.cs
@@ -1,6 +1,19 @@
-﻿namespace InterviewsAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InterviewsAPI.Models
 {
     public class Participant
     {
+        [Key]
+        public int ParticipantId { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string Email { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+        public int TimeToken { get; set; }
     }
 }
