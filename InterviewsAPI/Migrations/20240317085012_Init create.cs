0e31578868a2ce8a6ae074d5e2af493c23d8fdff
Alexey Taradoyna <AltarNatsev@yandex.ru>
2024-03-17 16:28:29 +0700

    Migration was performed using EF

---
+++ InterviewsAPI/Migrations/20240317085012_Init create.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace InterviewsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    ParticipantId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Score = table.Column<int>(type: "integer", nullable: false),
+                    TimeToken = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.ParticipantId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    QnId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    QnInWords = table.Column<string>(type: "varchar(250)", nullable: false),
+                    ImageName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Option1 = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Option2 = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Option3 = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Option4 = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Answer = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.QnId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Participants");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
---
+++ InterviewsAPI/Models/InterviewDbContect.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace InterviewsAPI.Models
+{
+    public class InterviewDbContect : DbContext
+    {
+        public InterviewDbContect(DbContextOptions<InterviewDbContect> options) : base(options) { }
+
+        public DbSet<Questions> Questions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=InterviewDb;Username=postgres;Password=kjh65gf");
+        }
+    }
+}
